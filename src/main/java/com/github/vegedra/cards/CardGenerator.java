5b1516294766f8a20aac3f3732703a25ab6ffacd
Pedro Ivo <98492435+vegedra@users.noreply.github.com>
2026-02-27 19:59:00 -0300

    Update CardGenerator.java

--- src/main/java/com/github/vegedra/cards/CardGenerator.java
+++ src/main/java/com/github/vegedra/cards/CardGenerator.java
@@ -1,3 +1,15 @@
+/*
+    Rigged Luck Copyright (C) Pedro Ivo Rocha de Deus / Digital Cake Studio - All Rights Reserved
+
+    This source code is protected under international copyright law.  
+    All rights reserved and protected by the copyright holders.
+    This file is confidential and only available to authorized individuals with the
+    permission of the copyright holders.  If you encounter this file and do not have
+    permission, please contact the copyright holders and delete this file.
+
+    Card Generator File
+*/
+
 package com.github.vegedra.cards;
 
 import java.util.Random;
@@ -6,6 +18,7 @@ public class CardGenerator {
 
     Random random = new Random();
 
+    // Gerar cartas para a roleta de acordo com Sorte atual
     public Card generateCard(int luck) {
 
         int roll = random.nextInt(100);
@@ -13,6 +26,7 @@ public class CardGenerator {
         // Sorte influencia
         roll += luck / 5;
 
+        // Carta fraca - TODO: Criar os tipos inspirados em tarot
         if (roll < 30) {
             return createWeakCard();
         }
@@ -24,6 +38,7 @@ public class CardGenerator {
         }
     }
 
+    // Criar cartas fracas - TODO: Adicionar mais cartas e sortear uma
     private Card createWeakCard() {
         return new Card(
                 "weak_click",
@@ -34,6 +49,7 @@ public class CardGenerator {
         );
     }
 
+    // Criar cartas normais
     private Card createNormalCard() {
         return new Card(
                 "normal_generator",
@@ -44,6 +60,7 @@ public class CardGenerator {
         );
     }
 
+    // Criar carta forte
     private Card createStrongCard() {
         return new Card(
                 "strong_click",
