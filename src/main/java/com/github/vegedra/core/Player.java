1dc4bbd80d44933d452c5e03dda6ea0e86fe40ad
Pedro Ivo <98492435+vegedra@users.noreply.github.com>
2026-02-27 19:52:46 -0300

    Update Player.java

--- src/main/java/com/github/vegedra/core/Player.java
+++ src/main/java/com/github/vegedra/core/Player.java
@@ -1,40 +1,50 @@
+/*
+    Rigged Luck Copyright (C) Pedro Ivo Rocha de Deus / Digital Cake Studio - All Rights Reserved
+
+    This source code is protected under international copyright law.  
+    All rights reserved and protected by the copyright holders.
+    This file is confidential and only available to authorized individuals with the
+    permission of the copyright holders.  If you encounter this file and do not have
+    permission, please contact the copyright holders and delete this file.
+
+    Player File
+*/
+
 package com.github.vegedra.core;
 
 import com.github.vegedra.cards.Card;
 
 public class Player {
 
+    // Atributos
     private int coins = 0;
-    private int clickValue = 1;
-    private int luck = 50;
+    private int clickValue = 1;    // Quantas moedas gera por clique
+    private int luck = 50;        // Sorte
 
-    public void addCoins(int amount) {
-        coins += amount;
-    }
-
+    // Getters
     public int getCoins() {
         return coins;
     }
-
-    public void addClickValue(int amount) {
-        clickValue += amount;
-    }
-
     public int getClickValue() {
         return clickValue;
     }
-
+    public int getLuck() {
+        return luck;
+    }
+    
+    // Setters
+    public void addCoins(int amount) {
+        coins += amount;
+    }
+    public void addClickValue(int amount) {
+        clickValue += amount;
+    }
     public void changeLuck(int amount) {
         luck += amount;
 
         if (luck > 100) luck = 100;
         if (luck < 0) luck = 0;
     }
-
-    public int getLuck() {
-        return luck;
-    }
-
     public void applyCard(Card c) {
         clickValue += c.value;
     }
