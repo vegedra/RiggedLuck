028f9838c300ccb59ab1670d25ab60b8eee8ddb6
Pedro Ivo <98492435+vegedra@users.noreply.github.com>
2026-03-20 19:52:26 -0300

    Update CardManager.java

--- src/main/java/com/github/vegedra/cards/CardManager.java
+++ src/main/java/com/github/vegedra/cards/CardManager.java
@@ -1,6 +1,15 @@
+/*
+    Copyright © 2026 [Pedro Ivo Rocha/Digital Cake Studio].
+    Todos os direitos reservados.
+    All rights reserved.
+
+    É proibida a reprodução, distribuição ou venda deste código
+    sem a permissão expressa do autor.
+
+    TO:DO Gameplay e mecanicas das cartas e seus efeitos
+*/
+
 package com.github.vegedra.cards;
 
 public class CardManager {
-
-
 }
